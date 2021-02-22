e604e63d6a23e35ef4ed7a7e49d8cb77424051ec
Antonio Francisco <afpb04@hotmail.com>
2021-02-22 11:24:26 -0300

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -26,6 +26,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
